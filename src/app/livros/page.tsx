3acaca6828c441f919d400ebd7d5947ec6594c45
ELIASHEAD <eliashead.costa@gmail.com>
2023-07-19 19:21:32 -0300

    feat: alinhar h1

--- src/app/livros/page.tsx
+++ src/app/livros/page.tsx
@@ -1,7 +1,7 @@
 function Livros() {
   return (
-    <main>
-      <div className="mt-16 flex min-h-screen items-center justify-center">
+    <main className="min-h-screen">
+      <div className="mt-16 flex items-center justify-center">
         <h1 className="text-5xl">Livros</h1>
       </div>
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl font-bold text-sky-600">Prof. Concursado</h1>
+      <h1 className="text-5xl font-bold text-sky-600">Prof. Concursada</h1>
     </main>
   )
 }
